0364796aa2e834b3b561c05b9a37a5388649b7ec
Ming-Hong Bai <mhbai@mail.naer.edu.tw>
2016-08-24 08:59:59 +0800

    設定 setup.py

--- setup.py
+++ setup.py
@@ -3,22 +3,14 @@
 from setuptools import setup, find_packages
 
 
-#with open('README.rst') as f:
-#    readme = f.read()
-
-#with open('LICENSE') as f:
-#    license = f.read()
-
 setup(
     name='naertk',
     version='1.0',
-    description='NAER package for NLP',
-    #long_description=readme,
+    description='NAER toolkits for NLP',
     author='Ming-Hong Bai',
     author_email='mhbai.tw@gmail.com',
     url='https://github.com/mhbai/naertk',
-    #license=license,
 	test_suite='tests',
-    #packages=find_packages(exclude=('tests', 'docs'))
+    packages=['dispersion'],
 )
 
